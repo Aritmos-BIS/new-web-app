f2b5af01338c68e7634fd1bfab570e1138395275
Salvador511 <salvadororozcogon511@gmail.com>
2024-07-05 15:32:17 -0600

    fix(AuthWrapper): I added the case of none session active

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -5,6 +5,10 @@ const AuthWrapper = ({ children }, pageType) =>{
   const router = useRouter()
   const { user } = useStore(state => state)
 
+  if(!user.userType){
+    router.push(`/auth/login/`)
+  }
+
   if (user.userType != pageType) {
     router.push(`/auth/${user.userType}/`)
   }
