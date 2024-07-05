057594781c7f84fe0456c6dadba21c96df0f2776
Salvador511 <salvadororozcogon511@gmail.com>
2024-07-05 15:23:00 -0600

    Feat(Wrapper): Developed AuthWrapper

---
+++ src/components/AuthWrapper.jsx
@@ -0,0 +1,16 @@
+import { useStore } from '@/libs/store'
+import { useRouter } from 'next/navigation';
+
+const AuthWrapper = ({ children }, pageType) =>{
+  const router = useRouter()
+  const { user } = useStore(state => state)
+
+  if (user.userType != pageType) {
+    router.push(`/auth/${user.userType}/`)
+  }
+
+
+  return <children/>
+}
+
+export default AuthWrapper
