bc55be4f9a28f6ec51cf79d38479e4fdc2b6a1e9
Salvador511 <salvadororozcogon511@gmail.com>
2024-07-05 22:32:02 -0600

    fix(AuthWrapper, professors and students): Added use client and fixed route of resend

--- src/app/auth/professors/page.jsx
+++ src/app/auth/professors/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Container, Typography, Box, Paper, Button, Grid } from '@mui/material';
