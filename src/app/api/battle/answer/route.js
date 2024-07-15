85d5de09856778fcb7d7b2e6604f73e3f389292d
Salvador511 <salvadororozcogon511@gmail.com>
2024-07-14 21:31:18 -0600

    "Update .gitignore to include .env file, and refactor src/app/api/battle/answer/route.js to remove unnecessary semicolons and add error handling"

--- src/app/api/battle/answer/route.js
+++ src/app/api/battle/answer/route.js
@@ -1,7 +1,7 @@
 import { mongoConnection } from '@/libs/mongo';
 import { NextResponse } from 'next/server';
 import Battle from '@/models/battle';
-import * as R from 'ramda';
+import * as R from 'ramda'; 
 
 export const GET = async (req) => {
   await mongoConnection();
@@ -13,23 +13,27 @@ export const GET = async (req) => {
   const { player1, player2 } = battle.toObject();
 
   try {
-    const answerPlayer1 = R.omit(['arimal'], player1);
-    const answerPlayer2 = R.omit(['arimal'], player2);
+
+    const answerPlayer1 = R.omit(['arimal'], player1)
+    const answerPlayer2 = R.omit(['arimal'], player2)
 
     if (!answerPlayer1 && !answerPlayer2) {
       return NextResponse.json({ error: 'players not found' }, { status: 404 });
     }
 
     return NextResponse.json({ turn: battle.turn, answerPlayer1, answerPlayer2 }, { status: 200 });
+
   } catch (error) {
     console.error('Error updating battle:', error);
     return NextResponse.json({ error: 'Failed to update battle' }, { status: 500 });
   }
-};
+
+
+}
 
 export const PUT = async (req) => {
   await mongoConnection();
-
+    
   const battles = await Battle.find();
   const battleId = battles.length - 1;
   const battle = battles[battleId];
@@ -37,20 +41,22 @@ export const PUT = async (req) => {
   const { player1, player2, ...body } = battle.toObject();
 
   const data = await req.json();
-  const { playerId } = data;
+  const { playerId } = data
 
-  let updatedBody;
+  let updatedBody
   if (playerId == battle.player1.playerId) {
     const { arimal } = player1;
     updatedBody = { ...body, player1: { ...data, arimal }, player2 };
   } else if (playerId == battle.player2.playerId) {
-    const { arimal } = player2;
+    const { arimal } = player2; 
     updatedBody = { ...body, player1, player2: { ...data, arimal } };
   } else {
     return NextResponse.json({ error: 'Player not found' }, { status: 404 });
   }
 
+
   try {
+    
     const updatedBattle = await Battle.findByIdAndUpdate(battle._id, updatedBody, { new: true });
 
     if (!updatedBattle) {
@@ -58,20 +64,9 @@ export const PUT = async (req) => {
     }
 
     return NextResponse.json({ body: updatedBody }, { status: 200 });
+
   } catch (error) {
     console.error('Error updating battle:', error);
     return NextResponse.json({ error: 'Failed to update battle' }, { status: 500 });
-  }
-};
-
-// Ensure that the API file is set up to handle these methods
-export default function handler(req, res) {
-  switch (req.method) {
-    case 'GET':
-      return GET(req, res);
-    case 'PUT':
-      return PUT(req, res);
-    default:
-      return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
